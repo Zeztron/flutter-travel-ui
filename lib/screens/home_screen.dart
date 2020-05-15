4ccccd100a728d9fa090ecc2846c4e4928041b0c
unknown <pateltharsh@gmail.com>
2020-05-15 18:07:19 -0400

    added icons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -8,8 +9,65 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  int _selectedIndex = 0;
+
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.bicycle
+  ];
+
+  Widget _buildIcon(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      child: Container (
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          color: _selectedIndex == index ? Theme.of(context).accentColor : Color(0xFFE7EBEE),
+          borderRadius: BorderRadius.circular(30.0)
+        ),
+        child: Icon(_icons[index], size: 25.0, color: _selectedIndex == index ? Theme.of(context).primaryColor : Color(0xFFB4C1C4)) 
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 30.0),
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 120.0),
+              child: Text('What would you like to find?', 
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold 
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _icons
+                .asMap()
+                .entries
+                .map(
+                  (MapEntry map) => _buildIcon(map.key)
+                )
+                .toList(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
